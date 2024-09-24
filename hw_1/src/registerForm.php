8d4528cd84dcb66173a5cabc652e7c2bfbc586b6
Danil Zaytsev <zaytsevdanil@yandex.ru>
2024-09-24 23:06:14 +0300

    hw1

---
+++ hw_1/src/registerForm.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "<html lang='en'>
+     <body>
+       <h1>register</h1>
+       <form method='post' action='register.php'>
+          <div>
+            <label for='firstName'>First name</label>
+           <input id='firstName' type='text' name='firstName' placeholder='Enter first name'>
+        </div>
+           <div>
+            <label for='lastName'>Last name</label>
+           <input id='lastName' type='text' name='lastName' placeholder='Enter last name'>
+        </div>
+       <div>
+            <label for='email'>email</label>
+           <input id='email' type='text' name='email' placeholder='Enter email'>
+        </div>
+       <div>
+            <label for='password'>password</label>
+           <input id='password' type='password' name='password' placeholder='Enter password'>
+        </div>
+        <button type='submit'>Login</button>
+       </form>
+     </body>
+   </html>";
