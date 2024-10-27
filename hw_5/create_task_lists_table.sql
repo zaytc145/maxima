fc4a67e1b1f036d873ac867670ae195d1590530d
Danil Zaytsev <zaytsevdanil@yandex.ru>
2024-10-27 23:33:14 +0300

    hw

---
+++ hw_5/create_task_lists_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE task_lists
+(
+    id    SERIAL PRIMARY KEY,
+    title VARCHAR(255)
+);
---
+++ hw_6/src/Models/Task.php
@@ -0,0 +1,9 @@
+<?php
+
+class Task extends Model
+{
+    public $id;
+    public $title;
+    public $description;
+    public $userId;
+}
